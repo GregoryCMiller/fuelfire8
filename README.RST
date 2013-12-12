b184a18d0a5d8f534598be2aa1e21d7678b70071
Greg Miller <gmill002@gmail.com>
2013-12-11 16:29:41 -0800

    add readme.rst

---
+++ README.RST
@@ -0,0 +1,4 @@
+fuelfire8
+=========
+
+helpers for FUELFIRE8 coupled fire and vegetation simulation
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version = "0.0.1",
     author = "Greg Miller",
     author_email = "gmill002@gmail.com",
-    description = "FUELFIRE8 helpers",
+    description = "FUELFIRE8 helpers.",
     packages = ["fuelfire8",],
     package_dir = {"fuelfire8": "fuelfire8",},
     install_requires = [],
